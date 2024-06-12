a15e28f6009549d7242cf080b447f0e9814f95f5
Eric Grierson <pigscanfly2k@gmail.com>
2024-06-11 21:05:43 -0600

    updates

--- WebpResizer/Program.cs
+++ WebpResizer/Program.cs
@@ -1,10 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using WebpResizer;
-//ImageResizer.ProcessDirectory(@"C:\Users\pigsc\OneDrive\デスクトップ\avatars\");
+//ImageResizer.ProcessDirectory(@"C:\Users\pigsc\OneDrive\デスクトップ\eggs\");
 //ImageSplitter.SplitImageIntoQuadrants(@"C:\Users\pigsc\OneDrive\デスクトップ\img\coins.webp");
 
 // Example usage
-string webpFilePath = @"C:\Users\pigsc\OneDrive\デスクトップ\img\treasurechest.webp";
-string pngFilePath = @"C:\Users\pigsc\OneDrive\デスクトップ\img\treasurechest.png";
-
-ImageConverter.ConvertWebPToPng(webpFilePath, pngFilePath);
+//string webpFilePath = @"C:\Users\pigsc\OneDrive\デスクトップ\img\treasurechest.webp";
+//string pngFilePath = @"C:\Users\pigsc\OneDrive\デスクトップ\img\treasurechest.png";
+//ImageConverter.ConvertWebPToPng(webpFilePath, pngFilePath);
