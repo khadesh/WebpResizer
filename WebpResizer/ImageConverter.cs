415fa052f0869a4206e89152a8e01b8bfc9c7b18
Eric Grierson <pigscanfly2k@gmail.com>
2024-06-15 15:03:06 -0600

    more updates for image work

--- WebpResizer/ImageConverter.cs
+++ WebpResizer/ImageConverter.cs
@@ -1,4 +1,5 @@
 ﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
 using SixLabors.ImageSharp.Formats.Png;
 using SixLabors.ImageSharp.Formats.Webp;
 
@@ -15,6 +16,15 @@ namespace WebpResizer
                 image.Save(pngFilePath, new PngEncoder());
             }
         }
+        public static void ConvertWebPToJpg(string webpFilePath, string pngFilePath)
+        {
+            // Load the WebP image
+            using (Image image = Image.Load(webpFilePath))
+            {
+                // Save the image as JPG
+                image.Save(pngFilePath, new JpegEncoder());
+            }
+        }
         public static void ConvertPngToWebP(string webpFilePath, string pngFilePath)
         {
             // Load the Png image
@@ -22,6 +32,32 @@ namespace WebpResizer
             {
                 // Save the image as PNG
                 image.Save(pngFilePath, new WebpEncoder());
+            }
+        }
+
+        public static void ProcessDirectoryToPng(string directoryPath, string toDirPath)
+        {
+            // Get all .webp files in the directory
+            var webpFiles = Directory.GetFiles(directoryPath, "*.webp");
+
+            // Call ResizeImage on each .webp file
+            foreach (var file in webpFiles)
+            {
+                Console.WriteLine($"Processing: {file}");
+                ConvertWebPToJpg(file, file.Replace(directoryPath, toDirPath).Replace(".webp", ".png"));
+            }
+        }
+
+        public static void ProcessDirectoryToJpg(string directoryPath, string toDirPath)
+        {
+            // Get all .webp files in the directory
+            var webpFiles = Directory.GetFiles(directoryPath, "*.webp");
+
+            // Call ResizeImage on each .webp file
+            foreach (var file in webpFiles)
+            {
+                Console.WriteLine($"Processing: {file}");
+                ConvertWebPToJpg(file, file.Replace(directoryPath, toDirPath).Replace(".webp", ".jpg"));
             }
         }
     }
--- WebpResizer/Program.cs
+++ WebpResizer/Program.cs
@@ -1,10 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using WebpResizer;
-//ImageResizer.ProcessDirectory(@"C:\Users\pigsc\OneDrive\デスクトップ\eggs\");
+//ImageResizer.ProcessDirectory(@"C:\Users\pigsc\OneDrive\デスクトップ\avatars\");
 //ImageSplitter.SplitImageIntoQuadrants(@"C:\Users\pigsc\OneDrive\デスクトップ\img\coins.webp");
 
 // Example usage
-string webpFilePath = @"C:\Users\pigsc\OneDrive\デスクトップ\img fix\ancient-dragon.webp";
-string pngFilePath = @"C:\Users\pigsc\OneDrive\デスクトップ\img fix\ancient-dragon.png";
+//string webpFilePath = @"C:\Users\pigsc\OneDrive\デスクトップ\img fix\zombiedragon5-512.webp";
+//string pngFilePath = @"C:\Users\pigsc\OneDrive\デスクトップ\img fix\zombiedragon5-512.png";
 //ImageConverter.ConvertWebPToPng(webpFilePath, pngFilePath);
-ImageConverter.ConvertPngToWebP(pngFilePath, webpFilePath);
+//ImageConverter.ConvertPngToWebP(pngFilePath, webpFilePath);
+
+
+ImageConverter.ProcessDirectoryToPng(@"C:\Users\pigsc\OneDrive\デスクトップ\cards\", @"C:\Users\pigsc\OneDrive\デスクトップ\cards\png\");
